457fd6ead60d5bf89872d436afb67db3fdc197b1
freddneos <fredd@neosdev.com.br>
2025-03-06 16:58:48 +0000

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cyw-2025/', // Replace 'cyw-2025' with your repository name
   plugins: [preact()],
   esbuild: {
     loader: 'jsx',
