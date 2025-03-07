215986435f5265e8ff590a157cda790401ca1118
freddneos <fredd@neosdev.com.br>
2025-03-07 22:09:39 +0000

    fix-vercel-deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cyw-2025/', // Replace 'cyw-2025' with your repository name
+  base: '/', // Replace 'cyw-2025' with your repository name
   plugins: [preact()],
   esbuild: {
     loader: 'jsx',
